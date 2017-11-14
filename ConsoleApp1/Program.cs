479337ab654dde3db125f1a2352c73f7f3b0e2fd
User <cfgcft>
2017-11-14 19:36:25 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "Matas";
+            var pavarde = "Lat";
+            var amzius = 20;
+            var ugis = 1.78;
+            var svoris = 75;
+            var aukstoji_mokykla = "nera";
+            var AkademinesGK = "nera";
+            var Kursas = "belenkoks";
+            var StudijuPP = "afigiena";
+            var AtsiskaitytuKS = "visi";
+            Console.WriteLine("vardas yra " + vardas);
+            Console.WriteLine("pavarde yra " + pavarde);
+            Console.WriteLine("amzius yra " + amzius);
+            Console.WriteLine("ugis yra " + ugis);
+            Console.WriteLine("svoris yra " + svoris);
+            Console.WriteLine("aukstoji_mokykla yra " + aukstoji_mokykla);
+            Console.WriteLine("AkademinesGK yra " + AkademinesGK);
+            Console.WriteLine("Kursas yra " + Kursas);
+            Console.WriteLine("StudijuPP yra " + StudijuPP);
+            Console.WriteLine("AtsiskaitytuKS yra " + AtsiskaitytuKS);
+            Console.ReadLine();
+        }
+    }
+}
